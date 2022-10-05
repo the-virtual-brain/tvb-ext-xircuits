ea0c9110f8489d5350372f9a9889eb76925dbc9a
popaula937 <paula.popa@codemart.ro>
2022-10-05 17:03:19 +0300

    WID-98. Print JSON-like version in output.

--- xai_components/utils.py
+++ xai_components/utils.py
@@ -64,16 +64,18 @@ def set_values(component, tvb_object):
 def print_component_summary(traited_obj):
     print('== COMPONENT SUMMARY ==')
     summary = traited_obj.summary_info()
+    print("{")
 
     # this if is needed because Connectivity doesn't add its type in its summary_info() method
     if TYPE_STR not in summary.keys():
         cls = type(traited_obj)
-        print(f'{TYPE_STR}: {cls.__name__}')
+        print(f'"{TYPE_STR}": "{cls.__name__}",')
 
     for attr_name, attr in summary.items():
-        print(f'{attr_name}: {attr}')
+        print(f'"{attr_name}": "{attr}",')
 
     # this if is needed because Connectivity doesn't add its gid in its summary_info() method
     if GID_STR not in summary.keys():
         gid_value = repr(traited_obj.gid)
-        print(f'{GID_STR}: {gid_value}')
+        print(f'"{GID_STR}": "{gid_value}"')
+    print("}")
