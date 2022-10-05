0f762a5f2ceab068c2ccb710424f3043744e5c80
popaula937 <paula.popa@codemart.ro>
2022-10-05 12:09:08 +0300

    WID-98. Server side part of the solution.

--- xircuits/handlers/__init__.py
+++ xircuits/handlers/__init__.py
@@ -1,7 +1,7 @@
 from jupyter_server.utils import url_path_join
 
 from .compile_xircuits import CompileXircuitsFileRouteHandler
-from .components import ComponentsRouteHandler
+from .components import ComponentsRouteHandler, EditXircuitsFile
 from .config import RunConfigRouteHandler
 from .debugger import DebuggerRouteHandler
 from .spark_submit import SparkSubmitRouteHandler
@@ -32,6 +32,10 @@ def setup_handlers(web_app, url_path):
         (
             url_path_join(base_url, url_path, "file/compile"),
             CompileXircuitsFileRouteHandler
+        ),
+        (
+            url_path_join(base_url, url_path, "components/edit"),
+            EditXircuitsFile
         )
 
     ])
