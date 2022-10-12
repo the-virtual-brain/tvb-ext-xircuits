6bc965c27cdd1db7726486339e04f65827b8f5f9
romina1601 <romina.baila16@gmail.com>
2022-10-12 12:36:57 +0300

    WID-95. Refactor backends and simulator according to PR suggestion

--- xai_components/xai_tvb_simulator/simulator.py
+++ xai_components/xai_tvb_simulator/simulator.py
@@ -4,7 +4,8 @@
 #
 # (c) 2022-2023, TVB Widgets Team
 #
-
+from tvb.datatypes.time_series import TimeSeries
+from tvb.simulator.backend.templates import MakoUtilMix
 from tvb.datatypes.connectivity import Connectivity
 from tvb.datatypes.cortex import Cortex
 from tvb.datatypes.patterns import SpatioTemporalPattern
@@ -29,16 +30,40 @@ class Simulator(Component):
     initial_conditions: InArg[list]
     monitors: InArg[list]
     simulation_length: InArg[float]
+    backend: InArg[MakoUtilMix]
 
-    simulator: OutArg[Simulator]
+    time_series: OutArg[TimeSeries]
 
     def __init__(self):
         set_defaults(self, self.Simulator)
+        self.backend = InArg(None)
+        self.time_series = OutArg(None)
 
     def execute(self, ctx) -> None:
+        # imports
+        from tvb.simulator.backend.nb_mpr import NbMPRBackend
+        from tvb.datatypes import time_series
+
         simulator = self.Simulator()
         set_values(self, simulator)
         simulator.configure()
 
-        self.simulator.value = simulator
-        print_component_summary(self.simulator.value)
+        print_component_summary(simulator)
+
+        # run simulation
+        backend = self.backend.value
+        if isinstance(backend, NbMPRBackend):
+            (time, data), = backend.run_sim(simulator, simulation_length=simulator.simulation_length)
+        else:
+            (time, data), = simulator.run()
+
+        # create TS
+        tsr = time_series.TimeSeriesRegion(
+            data=data,
+            time=time,
+            connectivity=simulator.connectivity,
+            sample_period=simulator.monitors[0].period / 1e3,
+            sample_period_unit='s')
+        tsr.configure()
+        print_component_summary(tsr)
+
