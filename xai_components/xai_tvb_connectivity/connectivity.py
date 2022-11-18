54ec2b94f7bfc678b6004833a52e19fc9e4d4ee1
romina1601 <romina.baila16@gmail.com>
2022-11-18 16:21:36 +0200

    WID-115. Add components for retrieving connectivities from siibra and using them inside simulation workflow

--- xai_components/xai_tvb_connectivity/connectivity.py
+++ xai_components/xai_tvb_connectivity/connectivity.py
@@ -32,3 +32,36 @@ class ConnectivityFromFile(Component):
 
         plt.imshow(self.connectivity.value.weights, interpolation='none')
         plt.show()
+
+
+@xai_component(color='rgb(85, 37, 130)')
+class ConnectivityFromSiibra(Component):
+    conn_from_siibra: InArg[dict]
+    subject_id: InArg[str]
+
+    connectivity: OutArg[Connectivity]
+
+    def __init__(self):
+        self.done = False
+        self.conn_from_siibra = InArg(None)
+        self.subject_id = InArg(None)
+        self.connectivity = OutArg(None)
+
+    def execute(self, ctx) -> None:
+        # imports
+        from matplotlib import pyplot as plt
+
+        conn_dict = self.conn_from_siibra.value
+        all_subjects = list(conn_dict.keys())   # list of all the subjects for which there's a connectivity
+        subject_id = self.subject_id.value
+        if not subject_id:
+            subject_id = all_subjects[0]    # if no subject id was provided, get the first connectivity
+        elif subject_id not in all_subjects:
+            raise ValueError(f'There is no connectivity for subject \'{subject_id}\' among the connectivities '
+                             f'that were provided')
+
+        self.connectivity.value = conn_dict[subject_id]
+        print_component_summary(self.connectivity.value)
+
+        plt.imshow(self.connectivity.value.weights, interpolation='none')
+        plt.show()
