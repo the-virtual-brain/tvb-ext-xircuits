ab61601c5b104d6bb609e498704d3992e5d23832
romina1601 <romina.baila16@gmail.com>
2022-11-24 15:57:26 +0200

    WID-115. Refactor siibra workflow to use only 1 component and rename Connectivity component category

--- xai_components/xai_connectivity/connectivity.py
+++ xai_components/xai_connectivity/connectivity.py
@@ -5,8 +5,8 @@
 # (c) 2022-2023, TVB Widgets Team
 #
 
+from tvb.adapters.creators import siibra_base as sb
 from tvb.datatypes.connectivity import Connectivity
-
 from xai_components.base import InArg, OutArg, Component, xai_component
 from xai_components.utils import print_component_summary
 
@@ -36,31 +36,32 @@ class ConnectivityFromFile(Component):
 
 @xai_component(color='rgb(85, 37, 130)')
 class ConnectivityFromSiibra(Component):
-    conn_from_siibra: InArg[dict]
+    atlas: InArg[str]
+    parcellation: InArg[str]
     subject_id: InArg[str]
 
     connectivity: OutArg[Connectivity]
 
     def __init__(self):
         self.done = False
-        self.conn_from_siibra = InArg(None)
-        self.subject_id = InArg(None)
+        self.atlas = InArg(sb.DEFAULT_ATLAS)
+        self.parcellation = InArg(sb.DEFAULT_PARCELLATION)
+        self.subject_id = InArg('000')
         self.connectivity = OutArg(None)
 
     def execute(self, ctx) -> None:
         # imports
         from matplotlib import pyplot as plt
 
-        conn_dict = self.conn_from_siibra.value
-        all_subjects = list(conn_dict.keys())   # list of all the subjects for which there's a connectivity
+        atlas = self.atlas.value
+        parcellation = self.parcellation.value
         subject_id = self.subject_id.value
-        if not subject_id:
-            subject_id = all_subjects[0]    # if no subject id was provided, get the first connectivity
-        elif subject_id not in all_subjects:
-            raise ValueError(f'There is no connectivity for subject \'{subject_id}\' among the connectivities '
-                             f'that were provided')
 
-        self.connectivity.value = conn_dict[subject_id]
+        sc_dict, _ = sb.get_connectivities_from_kg(atlas=atlas, parcellation=parcellation,
+                                                   subject_ids=subject_id,
+                                                   compute_fc=False)
+
+        self.connectivity.value = sc_dict[subject_id]
         print_component_summary(self.connectivity.value)
 
         plt.imshow(self.connectivity.value.weights, interpolation='none')
