8844985ac1b3aaaa83db95ea50d2a8191d592a64
romina1601 <romina.baila16@gmail.com>
2022-12-05 18:05:53 +0200

    WID-134. Upload files (provided as arguments to xircuits components) to HPC (wip)

--- xai_components/xai_connectivity/connectivity.py
+++ xai_components/xai_connectivity/connectivity.py
@@ -16,7 +16,7 @@ from xai_components.utils import print_component_summary
 
 @xai_component(color='rgb(85, 37, 130)')
 class ConnectivityFromFile(Component):
-    file_name: InArg[str]
+    file_path: InArg[str]
 
     connectivity: OutArg[Connectivity]
 
@@ -30,7 +30,7 @@ class ConnectivityFromFile(Component):
         from matplotlib import pyplot as plt
         from tvb.simulator.lab import connectivity
 
-        self.connectivity.value = connectivity.Connectivity.from_file(self.file_name.value)
+        self.connectivity.value = connectivity.Connectivity.from_file(self.file_path.value)
         print_component_summary(self.connectivity.value)
 
         plt.imshow(self.connectivity.value.weights, interpolation='none')
