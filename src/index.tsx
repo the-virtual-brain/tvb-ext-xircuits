2976fe97ee55f8b734cae4f1a29c346a6535488d
popaula937 <paula.popa@codemart.ro>
2022-12-15 18:09:19 +0200

    WID-130. Review and finalize venv workflow.

--- src/index.tsx
+++ src/index.tsx
@@ -265,7 +265,6 @@ const xircuits: JupyterFrontEndPlugin<void> = {
         code_str += `command_str= "${command_str}"\n`;
         code_str += "p=Popen(command_str, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)\n";
         code_str += "print('Remote Execution in process...\\n')\n";
-        code_str += `print('Please go to ${config['url']} for more details\\n')\n`;
         code_str += `print('${config['msg']}\\n')\n`;
         code_str += "for line in p.stdout:\n";
         code_str += "    " + "print(line.rstrip())\n\n";
