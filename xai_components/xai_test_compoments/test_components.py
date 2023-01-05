1eacb624f8ed997c377394e36144a0d652542abc
romina1601 <romina.baila16@gmail.com>
2023-01-05 15:35:53 +0200

    WID-140. Rename 'Numpy Array' literal to 'Literal Numpy Array'

--- xai_components/xai_test_compoments/test_components.py
+++ xai_components/xai_test_compoments/test_components.py
@@ -10,7 +10,7 @@ from xai_components.base import InArg, Component, xai_component
 
 
 @xai_component(color='rgb(85, 37, 130)')
-class ANumpyTest(Component):
+class NumpyTest(Component):
     numpy_code_str: InArg[numpy.ndarray]
 
     def __init__(self):
@@ -20,5 +20,4 @@ class ANumpyTest(Component):
     def execute(self, ctx) -> None:
         numpy_code = self.numpy_code_str.value
         print(f'Numpy code to be executed: {numpy_code}')
-        numpy_arr = eval(numpy_code)
-        print(f'Resulted numpy array: \n {numpy_arr}')
+        print(type(numpy_code))
