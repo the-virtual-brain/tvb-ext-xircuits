161736d9e8104cab32593c293c5f4467db5818b3
popaula937 <paula.popa@codemart.ro>
2023-01-12 16:12:17 +0200

    WID-145. Add documentation for StoreResults component.

--- xai_components/xai_storage/store_results.py
+++ xai_components/xai_storage/store_results.py
@@ -17,11 +17,13 @@ class StoreResults(Component):
     data_to_store: InCompArg[list]
     bucket_name: InArg[str]
     folder_path: InArg[str]
+    H5_format: InArg[bool]
 
     def __init__(self):
         self.done = False
         self.bucket_name = InArg(None)
         self.folder_path = InArg('')
+        self.H5_format = InArg(False)
 
     def execute(self, ctx) -> None:
         if self.bucket_name.value is not None:
@@ -40,22 +42,25 @@ class StoreResults(Component):
         os.mkdir(output_directory)
 
         for data_piece in self.data_to_store.value:
-            StoreFactory.store_data_piece(data_piece, output_directory)
+            StoreFactory.store_data_piece(data_piece, output_directory, self.H5_format.value)
 
 
 class StoreFactory(object):
 
     @staticmethod
-    def store_data_piece(data_piece, output_directory):
+    def store_data_piece(data_piece, output_directory, h5_format):
         from tvb.datatypes.time_series import TimeSeries
 
         if issubclass(type(data_piece), TimeSeries):
-            StoreFactory.store_time_series(data_piece, output_directory)
+            StoreFactory.store_time_series(data_piece, output_directory, h5_format)
         else:
             print(f"Cannot store data of type {type(data_piece)} yet. Please contact the development team for this.")
 
     @staticmethod
-    def store_time_series(time_series, output_directory):
-        ts_file_name = os.path.join(output_directory, f"timeseries_{time_series.title}.npy")
-        print(f"Storing timeseries for {time_series.title} monitor to {ts_file_name}...")
-        numpy.save(ts_file_name, time_series.data)
+    def store_time_series(time_series, output_directory, h5_format):
+        if h5_format:
+            print(f"Storing to H5 is not supported yet. Will be added soon...")
+        else:
+            ts_file_name = os.path.join(output_directory, f"timeseries_{time_series.title}.npy")
+            print(f"Storing timeseries for {time_series.title} monitor to {ts_file_name}...")
+            numpy.save(ts_file_name, time_series.data)
