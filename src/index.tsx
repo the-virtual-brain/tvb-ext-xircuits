d029a81520b63bed394b530c2f7f465925be1bef
popaula937 <paula.popa@codemart.ro>
2023-01-18 15:00:36 +0200

    WID-145. Use checkboxes for monitoring and stage-out.

--- src/index.tsx
+++ src/index.tsx
@@ -256,17 +256,17 @@ const xircuits: JupyterFrontEndPlugin<void> = {
       }
     };
 
-    function doRemoteRun(path: string, config){
+    function doRemoteRun(path: string, cfg){
 
       try {
-        let command_str = config['command'] + " " + path + " " + config['run_config_name'] + " " + config['project']
-            + " " + config['monitoring'];
+        let command_str = cfg['command'] + " " + path + " " + cfg['run_config_name'] + " " + cfg['project']
+            + " " + cfg['stage-out'];
         let code_str = "\nfrom subprocess import Popen, PIPE\n\n";
 
         code_str += `command_str= "${command_str}"\n`;
         code_str += "p=Popen(command_str, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)\n";
         code_str += "print('Remote Execution in process...\\n')\n";
-        code_str += `print('${config['msg']}\\n')\n`;
+        code_str += `print('${cfg['msg']}\\n')\n`;
         code_str += "for line in p.stdout:\n";
         code_str += "    " + "print(line.rstrip())\n\n";
         code_str += "if p.returncode != 0:\n";
