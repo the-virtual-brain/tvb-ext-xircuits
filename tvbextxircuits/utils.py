54ae69dc7544c8be7a0d980149f79e7a7d18f27f
popaula937 <paula.popa@codemart.ro>
2023-01-26 13:09:47 +0200

    WID-169. Use year first in dirname timestamp.

--- tvbextxircuits/utils.py
+++ tvbextxircuits/utils.py
@@ -2,3 +2,4 @@ STORAGE_CONFIG_FILE = 'storage_config.json'  # To be used only for HPC runs
 BUCKET_NAME_KEY = 'bucket_name'  # Used only for HPC runs
 FOLDER_PATH_KEY = 'folder_path'  # Used only for HPC runs
 STORE_RESULTS_DIR = 'results'  # Used by component that takes care of storing data and stage-out from HPC
+DIR_TIME_STAMP_FRMT = '%Y.%m.%d_%H:%M:%S'
