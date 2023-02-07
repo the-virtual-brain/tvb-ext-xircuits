758a20af8d2b02535b0e52626ff473c189f16237
popaula937 <paula.popa@codemart.ro>
2023-02-07 10:49:11 +0200

    WID-169. Fix tests.

--- tests/xircuits/test_notebook_generator.py
+++ tests/xircuits/test_notebook_generator.py
@@ -2,14 +2,15 @@ import os.path
 import shutil
 
 from tvbextxircuits import nb_generator
-from tvbextxircuits.nb_generator import NotebookGenerator, PhasePlaneNotebookGenerator
+from tvbextxircuits.nb_generator import NotebookGenerator, NotebookFactory
+from xai_components.xai_tvb_simulator.simulator import Simulator
 
 TEMP_DIR = "temp_test_dir"
 nb_generator.NOTEBOOKS_DIR = TEMP_DIR
 
 
 def test_notebook_generator():
-    notebook_generator = NotebookGenerator()
+    notebook_generator = NotebookGenerator(Simulator, '', {})
     assert os.path.exists(nb_generator.NOTEBOOKS_DIR)
     assert notebook_generator.notebook is not None
     assert len(notebook_generator.notebook['cells']) == 0
@@ -20,21 +21,23 @@ def test_notebook_generator():
     notebook_generator.add_markdown_cell("Hello world")
     assert len(notebook_generator.notebook['cells']) == 2
 
-    assert len(os.listdir(nb_generator.NOTEBOOKS_DIR)) == 0
-    notebook_path = notebook_generator.store('model', 'xircuits_id')
-    assert os.path.exists(notebook_path)
-
 
 def test_widget_code_generator():
-    widget_code_generator = PhasePlaneNotebookGenerator()
-    code = widget_code_generator.get_widget_code('Simulator', 'sim_id', 'xai_components/xai_tvb_simulator/simulator.py')
-    assert code is None
+    notebook_factory = NotebookFactory()
+    notebook = notebook_factory.get_notebook_for_component('Simulator', 'sim_id',
+                                                           'xai_components/xai_tvb_simulator/simulator.py', {})
+    assert notebook is None
 
-    code = widget_code_generator.get_widget_code('ReducedWongWang', 'model_id',
-                                                 'xai_components/xai_tvb_models/wong_wang.py')
-    assert code is not None
-    assert "models.ReducedWongWang()" in code
-    assert "w.export_filename = 'model_model_id'" in code
+    notebook_factory = NotebookFactory()
+    notebook = notebook_factory.get_notebook_for_component('ReducedWongWang', 'model_id',
+                                                           'xai_components/xai_tvb_models/wong_wang.py', {})
+    assert notebook is not None
+    assert "models.ReducedWongWang(**{})" in notebook.cells[2]['source']
+    assert "w.export_filename = 'model_model_id'" in notebook.cells[2]['source']
+
+    assert len(os.listdir(nb_generator.NOTEBOOKS_DIR)) == 0
+    notebook_path = notebook_factory.store(notebook, 'model', 'xircuits_id')
+    assert os.path.exists(notebook_path)
 
 
 def teardown_function():
