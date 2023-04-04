554ceca35d64d66e9ef993ff082f0524e3078e7a
romina1601 <romina.baila16@gmail.com>
2023-04-04 13:03:33 +0300

    WID-147. Fix Equation components not working with default values for 'parameters' port

--- xai_components/utils.py
+++ xai_components/utils.py
@@ -10,6 +10,7 @@ import numpy as np
 from inspect import isclass
 from tvb.basic.neotraits._attr import NArray, Final
 from tvb.basic.neotraits._core import HasTraits
+from tvb.datatypes.equations import Equation
 
 from xai_components.base import OutArg, InArg
 from xai_components.logger.builder import get_logger
@@ -41,7 +42,10 @@ def set_defaults(component, tvb_datatype):
     for attr in tvb_datatype.declarative_attrs:
         attr_value = getattr(tvb_datatype, attr)
         attr_default = attr_value.default
-        if isclass(attr_default) and issubclass(attr_value.field_type, HasTraits):
+        print(type(attr_default))
+        if issubclass(tvb_datatype, Equation) and callable(attr_default) and attr_default.__name__ == '<lambda>':
+            attr_default = attr_default()
+        elif isclass(attr_default) and issubclass(attr_value.field_type, HasTraits):
             attr_default = attr_default()
         if should_set_attr(attr_value):
             setattr(component, attr, InArg(attr_default))
